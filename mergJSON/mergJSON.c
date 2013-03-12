95e733620b5d765903a2ce6eeba0a5d124193e1a
Monte Goulding <monte@sweattechnologies.com>
2013-03-12 21:47:44 +1100

    added JSON_SORT_KEYS to the pretty print option. object keys will now be sorted. also fixed some Xcode project issues

--- mergJSON/mergJSON.c
+++ mergJSON/mergJSON.c
@@ -275,7 +275,7 @@ LIVECODE_FUNCTION(mergJSONEncode)
     }
     size_t tFlags = JSON_ENCODE_ANY;
     if (tPretty) {
-        tFlags = tFlags | JSON_INDENT(2);
+        tFlags = tFlags | JSON_INDENT(2) | JSON_SORT_KEYS;
     } else {
         tFlags = tFlags | JSON_COMPACT;
     }
